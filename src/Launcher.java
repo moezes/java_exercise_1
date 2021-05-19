f77d830d1988e114d24d81f98480b8bbaa45252f
M H <mohamad.al-hawari@epita.fr>
2021-05-19 02:08:51 +0200

    added input loop to launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,10 +1,13 @@
 public class Launcher {
     public static void main(String[] args) {
         System.out.println(("I have no idea what I am doing but I'm trying my best ;-;"));
-        var input = new java.util.Scanner(System.in);
-        var newInput = input.nextLine();
-        if (!newInput.equals("quit")) {
+
+        var scan = new java.util.Scanner(System.in);
+        var input = scan.nextLine();
+
+        while (!input.equals("quit")) {
             System.out.println("Unknown command");
+            input = scan.nextLine();
         }
     }
 }
